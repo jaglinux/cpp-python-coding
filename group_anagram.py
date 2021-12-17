f94ed1ab17dfd1e5ddc14d5b5b83b032120b668b
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2021-12-17 11:56:41 -0800

    Create group_anagram.py

---
+++ group_anagram.py
@@ -0,0 +1,14 @@
+words = [ "eat","tea","tan","ate","nat","bat"]
+h = {}
+result=[]
+
+for word in words:
+    s_word = ''.join(sorted(word))
+    if s_word not in h:
+        h[s_word] = []
+    h[s_word].append(word)
+
+for v in h.values():
+    result.append(v)
+
+print(result)
