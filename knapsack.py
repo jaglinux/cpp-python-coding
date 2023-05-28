e70fe11ffb6d58bd99bb8e53fbe00ccc77702bb1
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-05-28 10:01:25 -0700

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -1,13 +1,18 @@
 def knapSack(W, weight, profit, n):
+    
     def rec(index, w):
-        print(index, w)
         if index == n:
             return 0
+        
+        if dp[index][w] != -1:
+            return dp[index][w]
         result1 = 0 + rec(index+1, w)
         result2 = 0
         if w - weight[index] >= 0:
             result2 = profit[index] + rec(index+1, w-weight[index])
-        return max(result1, result2)
+        dp[index][w] = max(result1, result2)
+        return dp[index][w]
+    
     return rec(0, W)
 
 if __name__ == '__main__':
@@ -15,6 +20,7 @@ if __name__ == '__main__':
     weight = [10, 20, 30]
     W = 50
     n = len(profit)
+    dp = [[-1]*(W+1) for _ in range(n)]
     print(knapSack(W, weight, profit, n))
 
-# answer is 220
+# 220
