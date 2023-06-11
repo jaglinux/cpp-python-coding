ed8ed63c44ebd88f5faad592a6eaf6e72eac6fc3
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2023-06-10 22:22:03 -0700

    Update group_anagram.py

--- group_anagram.py
+++ group_anagram.py
@@ -13,3 +13,17 @@ for v in h.values():
 
 print(result)
 # [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+# words = [ "eat","tea","tan","ate","nat","bat"]
+# h = {}
+# result=[]
+
+# for word in words:
+#     s_word = tuple(sorted(word))
+#     if s_word not in h:
+#         h[s_word] = []
+#     h[s_word].append(word)
+
+# for v in h.values():
+#     result.append(v)
+
+# print(result)
